e0ac0106f43213aeab69b6dd79948974e36470e3
taku nemoto <nemoto@crypton.co.jp>
2024-06-26 09:57:21 +0900

    テーマやMVI用のファイルコミット

---
+++ app/src/main/java/net/chasmine/ninjalauncher/model/AppModel.kt
@@ -0,0 +1,8 @@
+package net.chasmine.ninjalauncher.model;
+
+data class AppModel(
+        val id: String,
+        val name: String,
+        val packageName: String,
+        val icon: String // アイコンのリソースパス
+)
